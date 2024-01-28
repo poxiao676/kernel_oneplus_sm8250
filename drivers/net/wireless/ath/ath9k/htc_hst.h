abdacfd4c4a904ad0d56f721ed0940ebfabdd6fc
poxiao676 <2665801232@qq.com>
2024-01-28 22:47:48 +0000

    Fix ath9k naming conflict

--- drivers/net/wireless/ath/ath9k/htc_hst.h
+++ drivers/net/wireless/ath/ath9k/htc_hst.h
@@ -203,7 +203,7 @@ struct htc_comp_msg {
 } __packed;
 
 int htc_init(struct htc_target *target);
-int htc_connect_service(struct htc_target *target,
+int htc_connect_service_hst(struct htc_target *target,
 			  struct htc_service_connreq *service_connreq,
 			  enum htc_endpoint_id *conn_rsp_eid);
 int htc_send(struct htc_target *target, struct sk_buff *skb);
